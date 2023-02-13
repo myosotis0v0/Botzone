cfd0ab4b6efd5eefbe8d6cb7c09f7ca45e1512c9
myosotis0v0 <1243576876@qq.com>
2023-02-13 13:10:12 +0800

    backend test

--- backend/src/main/java/com/botzone/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/botzone/backend/controller/pk/BotInfoController.java
@@ -3,11 +3,25 @@ package com.botzone.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "Bot1";
+    public List<Map<String, String>> getBotInfo(){
+        List<Map<String, String>> list = new LinkedList<>();
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "bot1");
+        bot1.put("rating", "1500");
+        list.add(bot1);
+        Map<String, String> bot2 = new HashMap<>();
+        bot2.put("name", "bot2");
+        bot2.put("rating", "2500");
+        list.add(bot2);
+        return list;
     }
 }
